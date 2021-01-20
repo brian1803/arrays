3f2d58bf76fff5d6e85659dbcf2437c313be9a3a
brian1803 <134600@students.nccvt.k12.de.us>
2021-01-20 20:30:27 +0000

    task 1.3

--- prompt.js
+++ prompt.js
@@ -3,4 +3,10 @@ let songs ["song1", "song2", "song3", "song4", "song5"]
 console.log(songs[0])
 console.log(songs[2])
 
-songs
+songs[4] = "happy";
+
+let jamFour =songs[3];
+
+songs.push("africa")
+
+songs.unshift("salad days")
